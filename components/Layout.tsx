f2590016f3814a452a04901b441f1bdb310405d9
idanhahn <idanhahn@gmail.com>
2022-08-02 17:10:55 +0300

    ts fix for deploy 3

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import TopBar from './TopBar';
 
-import styles from './layout.module.css';
+import styles from './Layout.module.css';
 import Box from '@mui/material/Box';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
